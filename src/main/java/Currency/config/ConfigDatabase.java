c1c4fe6349c11fffb3f8c2971503cbb70dd28ceb
LENOVO <luxus0@gmail.com>
2020-12-13 21:58:36 +0100

    Initial commit

---
+++ src/main/java/Currency/config/ConfigDatabase.java
@@ -0,0 +1,13 @@
+package Currency.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:application-development.properties"),
+        @PropertySource("classpath:application-production.properties")
+})
+public class ConfigDatabase {
+}
