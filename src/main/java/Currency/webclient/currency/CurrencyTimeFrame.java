655e9697ec9eab92c09454882b752212eed844ba
LENOVO <luxus0@gmail.com>
2020-12-14 22:44:11 +0100

    not finished class

---
+++ src/main/java/Currency/webclient/currency/CurrencyTimeFrame.java
@@ -0,0 +1,12 @@
+package Currency.webclient.currency;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrencyTimeFrame {
+
+    public void getTimeFrameCurrency()
+    {
+
+    }
+}
