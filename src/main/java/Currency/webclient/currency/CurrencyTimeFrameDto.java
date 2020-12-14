65b345ed669b90e208fc223a991b812ac51f8409
LENOVO <luxus0@gmail.com>
2020-12-14 22:33:46 +0100

    add Class CurrencyTimeFrameDto

---
+++ src/main/java/Currency/webclient/currency/CurrencyTimeFrameDto.java
@@ -0,0 +1,20 @@
+package Currency.webclient.currency;
+
+import Currency.model.CurrencyDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CurrencyTimeFrameDto{
+
+    private final Long id;
+    private final Set<CurrencyDto> currencies;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+}
