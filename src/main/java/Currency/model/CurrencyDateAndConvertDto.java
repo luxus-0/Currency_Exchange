aaa32bdd3dba85b7844bcd8a6c720ad3b9b4b60e
LENOVO <luxus0@gmail.com>
2020-12-15 21:53:09 +0100

    create class CurrencyDateAndConvertDto

---
+++ src/main/java/Currency/model/CurrencyDateAndConvertDto.java
@@ -0,0 +1,14 @@
+package Currency.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+public class CurrencyDateAndConvertDto {
+    private final Set<CurrencyConverterDto> currencyConverterDto;
+    private final Set<LocalDate> currencyDateDto;
+}
