8dda6d7b0dc2f1905fa75af814be9af2fad71484
LENOVO <luxus0@gmail.com>
2020-12-15 21:51:48 +0100

    delete id

--- src/main/java/Currency/webclient/currency/CurrencyConverterCreator.java
+++ src/main/java/Currency/webclient/currency/CurrencyConverterCreator.java
@@ -6,7 +6,6 @@ public class CurrencyConverterCreator {
 
     public CurrencyConverterDto create(CurrencyConverterDto converterDto) {
         return CurrencyConverterDto.builder()
-                .id(converterDto.getId())
                 .from(converterDto.getFrom())
                 .to(converterDto.getTo())
                 .amount(converterDto.getAmount())
