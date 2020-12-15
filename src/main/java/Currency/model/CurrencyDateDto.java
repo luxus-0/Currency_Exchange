cb85bfec27bc3acb69589cda7fc27fac9cbe8bda
LENOVO <luxus0@gmail.com>
2020-12-15 19:36:34 +0100

    create function get()

--- src/main/java/Currency/model/CurrencyDateDto.java
+++ src/main/java/Currency/model/CurrencyDateDto.java
@@ -1,6 +1,8 @@
 package Currency.model;
 
+import Currency.exception.CurrencyDateNotFoundException;
 import lombok.*;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -11,8 +13,26 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
+@Log4j2
 public class CurrencyDateDto {
 
-    private Set<LocalDate> currencyDate;
+    private Set<LocalDate> currenciesDateDto;
     private CurrencyConverterDto converterDto;
+
+    public Set<LocalDate> get()
+    {
+        currenciesDateDto = Set.of(LocalDate.now(),LocalDate.of(2020,11,7));
+        CurrencyConverterDto convert = new CurrencyConverterDto(1L,"USD","EUR",12);
+        CurrencyDateDto dateDto = CurrencyDateDto.builder()
+                .currenciesDateDto(currenciesDateDto)
+                .converterDto(convert)
+                .build();
+        currenciesDateDto.stream()
+                .filter(p -> p.getYear() > 1900)
+                .findAny()
+                .orElseThrow(() -> new CurrencyDateNotFoundException("currency date not found!!"));
+        log.info(currenciesDateDto.size());
+
+        return currenciesDateDto;
+    }
 }
