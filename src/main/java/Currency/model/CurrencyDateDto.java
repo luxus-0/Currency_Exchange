25839b9d670bdc870dad1bd341b18112ea5a4c4b
LENOVO <luxus0@gmail.com>
2020-12-15 20:49:56 +0100

    add private variables and Set currenciesDateDto,currenciesConverterDto

--- src/main/java/Currency/model/CurrencyDateDto.java
+++ src/main/java/Currency/model/CurrencyDateDto.java
@@ -1,7 +1,13 @@
 package Currency.model;
 
 import Currency.exception.CurrencyDateNotFoundException;
-import lombok.*;
+import Currency.webclient.currency.CurrencyConvert;
+import Currency.webclient.currency.CurrencyConverterCreator;
+import Currency.webclient.currency.CurrencyDateCreator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
@@ -17,22 +23,24 @@ import java.util.Set;
 public class CurrencyDateDto {
 
     private Set<LocalDate> currenciesDateDto;
-    private CurrencyConverterDto converterDto;
+    private Set<CurrencyConverterDto> currenciesConverterDto;
+    private CurrencyConvert currencyConvert;
+    private CurrencyDateCreator dateCreator;
+    private CurrencyConverterCreator converterCreator;
 
-    public Set<LocalDate> get()
-    {
-        currenciesDateDto = Set.of(LocalDate.now(),LocalDate.of(2020,11,7));
-        CurrencyConverterDto convert = new CurrencyConverterDto(1L,"USD","EUR",12);
-        CurrencyDateDto dateDto = CurrencyDateDto.builder()
-                .currenciesDateDto(currenciesDateDto)
-                .converterDto(convert)
-                .build();
+    public Set<LocalDate> get(LocalDate date,String from,String to,Float amount) throws Exception {
+        currenciesDateDto = Set.of(date);
+        currenciesConverterDto = Set.of(currencyConvert.convert(from,to,amount));
+        dateCreator.create(currenciesDateDto);
+        converterCreator.create(currencyConvert.convert(from,to,amount));
         currenciesDateDto.stream()
                 .filter(p -> p.getYear() > 1900)
                 .findAny()
                 .orElseThrow(() -> new CurrencyDateNotFoundException("currency date not found!!"));
-        log.info(currenciesDateDto.size());
-
+        currenciesConverterDto.stream()
+                .filter(p -> p.getFrom().equals(from) && p.getTo().equals(to) && p.getAmount() > 0)
+                .findAny()
+                .orElseThrow(() -> new CurrencyDateNotFoundException("currency date not found!!"));
         return currenciesDateDto;
     }
 }
