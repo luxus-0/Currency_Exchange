f1a00bb5a25196a3cb3bbd4c099989b872f9762d
LENOVO <luxus0@gmail.com>
2020-12-15 18:43:24 +0100

    getUrlDateCurrency() change url

--- src/main/java/Currency/webclient/currency/CurrencyUrl.java
+++ src/main/java/Currency/webclient/currency/CurrencyUrl.java
@@ -11,10 +11,7 @@ public class CurrencyUrl {
     {
         return "http://api.currencylayer.com/";
     }
-    public String getUrlLiveCurrency()
-    {
-        return getUrl() +"live?";
-    }
+    public String getUrlLiveCurrency() { return getUrl() +"live?"; }
     public String getUrlDateCurrency() { return getUrl() +"historical"+ getKey()+"/date={date}"; }
     public String getUrlSourceAndAmountCurrency() { return getUrl() + getUrlDateCurrency() + getKey() + "&source={source}" +"&amount={amount}";}
     public String getUrlTimeFrameCurrency(){ return getUrl() +"timeframe" +getKey() +" &currencies={currencies}&startDate ={startDate}&endDate={endDate}";};
