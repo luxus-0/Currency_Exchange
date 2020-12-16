d22fca8bc0f2c5babd0c1000709238e01d477d5c
LENOVO <luxus0@gmail.com>
2020-12-16 19:35:05 +0100

    create class CurrencyDateCreator

--- src/main/java/Currency/webclient/currency/CurrencyDateCreator.java
+++ src/main/java/Currency/webclient/currency/CurrencyDateCreator.java
@@ -2,10 +2,14 @@ package Currency.webclient.currency;
 
 import Currency.model.CurrencyDateDto;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Set;
 
+@Service
 @AllArgsConstructor
 public class CurrencyDateCreator {
 
