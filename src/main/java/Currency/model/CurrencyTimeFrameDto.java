1e9eebf3255373cb4298f7cad67e9f02d7538156
LENOVO <luxus0@gmail.com>
2020-12-16 21:20:53 +0100

    add class CurrencyTimeFrameDto

--- src/main/java/Currency/model/CurrencyTimeFrameDto.java
+++ src/main/java/Currency/model/CurrencyTimeFrameDto.java
@@ -1,25 +1,22 @@
 package Currency.model;
 
 import Currency.model.CurrencyDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.stereotype.Component;
 
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.Set;
 
-@AllArgsConstructor
+@Component
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyTimeFrameDto{
-
-    private final Long id;
-    private final Set<CurrencyDto> currencies;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-
-    public void get()
-    {
-
-    }
+    @Id
+    private Long id;
+    private Set<String> currenciesTimeFrame;
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
