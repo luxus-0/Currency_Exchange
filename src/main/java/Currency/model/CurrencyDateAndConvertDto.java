93285e0eb68e3c62c6ce3bab81c1d0a10f5bc4e3
LENOVO <luxus0@gmail.com>
2020-12-16 19:34:39 +0100

    add @Component

--- src/main/java/Currency/model/CurrencyDateAndConvertDto.java
+++ src/main/java/Currency/model/CurrencyDateAndConvertDto.java
@@ -2,9 +2,12 @@ package Currency.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.util.Set;
 
+@Component
 @AllArgsConstructor
 @Getter
 public class CurrencyDateAndConvertDto {
