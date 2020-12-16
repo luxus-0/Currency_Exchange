eca4783ff0100c47ab7c6a8d200fc585e5695ae3
LENOVO <luxus0@gmail.com>
2020-12-16 19:42:04 +0100

    add @Component

--- src/main/java/Currency/webclient/currency/CurrencyDateAndConvert.java
+++ src/main/java/Currency/webclient/currency/CurrencyDateAndConvert.java
@@ -39,5 +39,4 @@ public class CurrencyDateAndConvert {
         log.info(currenciesConverterDto);
         return new CurrencyDateAndConvertDto(currenciesConverterDto,currenciesDateDto);
     }
-
 }
