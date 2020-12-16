3226848ed4012b037d7d8a4423e18f6e24fdc038
LENOVO <luxus0@gmail.com>
2020-12-16 19:30:38 +0100

    add function create

--- src/main/java/Currency/webclient/currency/CurrencyConverterCreator.java
+++ src/main/java/Currency/webclient/currency/CurrencyConverterCreator.java
@@ -1,7 +1,9 @@
 package Currency.webclient.currency;
 
 import Currency.model.CurrencyConverterDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CurrencyConverterCreator {
 
     public CurrencyConverterDto create(CurrencyConverterDto converterDto) {
