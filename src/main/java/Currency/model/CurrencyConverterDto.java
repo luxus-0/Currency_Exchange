f59761bdb2b235aba712eda368a7f53dd1db7275
LENOVO <luxus0@gmail.com>
2020-12-18 00:11:11 +0100

    add final field

--- src/main/java/Currency/model/CurrencyConverterDto.java
+++ src/main/java/Currency/model/CurrencyConverterDto.java
@@ -3,16 +3,12 @@ package Currency.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class CurrencyConverterDto {
-    private  String from;
-    private  String to;
-    private  Float amount;
+    private final String from;
+    private final String to;
+    private final Float amount;
 }
