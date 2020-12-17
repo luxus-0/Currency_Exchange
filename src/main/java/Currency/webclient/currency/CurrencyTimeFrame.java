1bd3d7a587857781fffc251cf3d1dc9c81160991
LENOVO <luxus0@gmail.com>
2020-12-18 00:33:52 +0100

    not finished method getCurrencyTimeFrame

--- src/main/java/Currency/webclient/currency/CurrencyTimeFrame.java
+++ src/main/java/Currency/webclient/currency/CurrencyTimeFrame.java
@@ -3,17 +3,14 @@ package Currency.webclient.currency;
 import Currency.model.CurrencyTimeFrameDto;
 import Currency.validation.CurrencyTimeFrameValid;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 @Service
 @AllArgsConstructor
-@Log4j2
 public class CurrencyTimeFrame {
 
     private final CurrencyTimeFrameCreator creator;
@@ -26,10 +23,6 @@ public class CurrencyTimeFrame {
         currencyTimeFrameValid.valid(startDate, endDate);
         Set<String> currenciesTimeFrame = Set.of(currencies);
         creator.createCurrencyTimeFrame(startDate,endDate);
-        String sql = "SELECT * FROM CurrencyTimeFrameDto";
-        jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(CurrencyTimeFrameDto.class));
-        log.info(sql);
-        log.info(currenciesTimeFrame);
 
         return currencyTimeFrameDto;
     }
