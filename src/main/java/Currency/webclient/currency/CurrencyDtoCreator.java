1058799ff8112efa1f39253792489610b9969a8f
LENOVO <luxus0@gmail.com>
2020-12-18 15:43:25 +0100

    change for 2 parameter in method

--- src/main/java/Currency/webclient/currency/CurrencyDtoCreator.java
+++ src/main/java/Currency/webclient/currency/CurrencyDtoCreator.java
@@ -8,7 +8,7 @@ import java.util.Set;
 @Service
 public class CurrencyDtoCreator {
 
-    public CurrencyDto create(Set<String> currency, Float amount, String source)
+    public CurrencyDto create(Float amount, String source)
     {
         return CurrencyDto.builder()
                 .amount(amount)
