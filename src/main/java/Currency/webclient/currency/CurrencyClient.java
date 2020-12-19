e699f7f486309bbf1ee80b2c77289cd701243450
LENOVO <luxus0@gmail.com>
2020-12-19 18:43:33 +0100

    add RestTemplate

--- src/main/java/Currency/webclient/currency/CurrencyClient.java
+++ src/main/java/Currency/webclient/currency/CurrencyClient.java
@@ -3,22 +3,21 @@ package Currency.webclient.currency;
 import Currency.exception.CurrencySourceNotFoundException;
 import Currency.model.CurrencyDto;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Log4j2
 @AllArgsConstructor
+@Log4j2
 public class CurrencyClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
     private final CurrencyUrl currencyUrl;
     private final CurrencyDtoCreator currencyDtoCreator;
 
-    public CurrencyDto getCurrencyAmountWithSource(String currency,Float amount, String source)
+    public CurrencyDto getCurrencyAmountWithSource(String currency, Float amount, String source)
     {
        CurrencyDto currencyDto =  callUsd(currencyUrl.getUrlAmountWithSourceCurrency(),
                 CurrencyDto.class,
@@ -40,6 +39,7 @@ public class CurrencyClient {
     }
 
     public <T> T callUsd(String url, Class<T> reponseType, Object...objects) {
+        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(currencyUrl.getUrl() + url, reponseType, objects);
     }
 
