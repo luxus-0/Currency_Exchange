e91368bf89ebcf0eb9b1cf31ea29577bef128f1a
LENOVO <luxus0@gmail.com>
2020-12-19 20:22:37 +0100

    Initial commit

--- src/main/java/Currency/webclient/currency/CurrencyDateAndConvert.java
+++ src/main/java/Currency/webclient/currency/CurrencyDateAndConvert.java
@@ -3,6 +3,7 @@ package Currency.webclient.currency;
 import Currency.exception.CurrencyDateNotFoundException;
 import Currency.model.CurrencyConverterDto;
 import Currency.model.CurrencyDateAndConvertDto;
+import Currency.model.CurrencyDateDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,7 +16,7 @@ import java.util.Set;
 @Log4j2
 public class CurrencyDateAndConvert {
 
-    private Set<LocalDate> currenciesDateDto;
+    private final Set<LocalDate> currenciesDateDto;
     private Set<CurrencyConverterDto> currenciesConverterDto;
     private final CurrencyConvert currencyConvert;
     private final CurrencyDateCreator dateCreator;
@@ -23,21 +24,20 @@ public class CurrencyDateAndConvert {
 
 
     public CurrencyDateAndConvertDto getCurrencyDateWithConverter(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, String from, String to, Float amount) throws Exception {
-        currenciesDateDto = Set.of(date);
         currenciesConverterDto = Set.of(currencyConvert.convert(from,to,amount));
-        dateCreator.create(currenciesDateDto);
+        dateCreator.create(Set.of(date));
         converterCreator.create(currencyConvert.convert(from,to,amount));
+
         currenciesDateDto.stream()
                 .filter(p -> p.getYear() > 1900)
                 .findAny()
                 .orElseThrow(() -> new CurrencyDateNotFoundException("currency date not found!!"));
+
         currenciesConverterDto.stream()
                 .filter(p -> p.getFrom().equals(from) && p.getTo().equals(to) && p.getAmount() > 0)
                 .findAny()
                 .orElseThrow(() -> new CurrencyDateNotFoundException("currency converter not found!!"));
 
-        log.info(currenciesDateDto);
-        log.info(currenciesConverterDto);
         return new CurrencyDateAndConvertDto(currenciesConverterDto,currenciesDateDto);
     }
 }
