7e1c6d67982d0cde0904f9d7d3be4586eec3ca6e
LENOVO <luxus0@gmail.com>
2020-12-19 19:57:05 +0100

    Initial commit

--- src/main/java/Currency/webclient/currency/CurrencyDtoCreator.java
+++ src/main/java/Currency/webclient/currency/CurrencyDtoCreator.java
@@ -3,10 +3,12 @@ package Currency.webclient.currency;
 import Currency.model.CurrencyDto;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class CurrencyDtoCreator {
 
-    public CurrencyDto create(Float amount, String source)
+    public CurrencyDto create(Set<String> currencies, Float amount, String source)
     {
         return CurrencyDto.builder()
                 .amount(amount)
