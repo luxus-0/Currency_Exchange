c9ee38b4e5cd05c08a1b2aedacc11c2786584d2b
LENOVO <luxus0@gmail.com>
2020-12-19 19:10:51 +0100

    add creator and return CurrencyDateDto

--- src/main/java/Currency/webclient/currency/CurrencyDate.java
+++ src/main/java/Currency/webclient/currency/CurrencyDate.java
@@ -1,12 +1,14 @@
 package Currency.webclient.currency;
 
 import Currency.exception.CurrencyDateNotFoundException;
+import Currency.model.CurrencyDateDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -14,17 +16,20 @@ import java.time.LocalDate;
 public class CurrencyDate {
 
     private final CurrencyUrl url;
+    private final CurrencyDateCreator currencyDateCreator;
+    private final CurrencyDateDto currencyDateDto;
 
-    public LocalDate getCurrencyDate(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    public CurrencyDateDto getCurrencyDate(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         if(url.getUrlDateCurrency().isEmpty() && date == null)
         {
             throw new CurrencyDateNotFoundException("Currency date is empty");
         }
         else
         {
+            currencyDateCreator.create(Set.of(date));
             JSONObject currencyDate = new JSONObject(date);
             log.info(currencyDate);
         }
-        return date;
+        return currencyDateDto;
     }
 }
