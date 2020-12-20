763d2ca95f5ee01687709668f68620ff25df0525
LENOVO <luxus0@gmail.com>
2020-12-20 21:28:02 +0100

    add method getUrlIpStack

--- src/main/java/Currency/webclient/currency/CurrencyUrl.java
+++ src/main/java/Currency/webclient/currency/CurrencyUrl.java
@@ -1,11 +1,22 @@
 package Currency.webclient.currency;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 @Component
+@AllArgsConstructor
 public class CurrencyUrl {
 
+    private final CurrencyWithIpStack currencyWithIpStack;
+
     public String getUrl() { return "http://api.currencylayer.com/"; }
+    public String getUrlIpStack() throws Exception
+    {
+        return "https://api.ipstack.com/" + Arrays.toString(currencyWithIpStack.getIpAddress()) + getKey();
+    }
     public String getUrlLiveCurrency() { return getUrl() +"live"; }
     public String getUrlDateCurrency() { return getUrl() +"historical"+ getKey()+"/date={date}"; }
     public String getUrlAmountWithSourceCurrency() { return getUrl() + getUrlDateCurrency() + getKey() + "&source={source}" +"&amount={amount}";}
