56b88e3976d75280e87363e45691f1486db22174
LENOVO <luxus0@gmail.com>
2020-12-20 21:05:51 +0100

    add method getIpStack

--- src/main/java/Currency/api/IpStackApi.java
+++ src/main/java/Currency/api/IpStackApi.java
@@ -4,9 +4,7 @@ import Currency.webclient.currency.CurrencyWithIpStack;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @RestController
 @AllArgsConstructor
@@ -15,7 +13,7 @@ public class IpStackApi {
     private final CurrencyWithIpStack ipStack;
 
     @GetMapping("/currencyIp")
-    public InetAddress getIpStack() throws UnknownHostException {
+    public InetAddress getIpStack() throws Exception {
         return ipStack.showIpAddress();
     }
 }
