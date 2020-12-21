9adb9cccd2f99c7c8414308215cb4fe3868854f3
LENOVO <luxus0@gmail.com>
2020-12-21 16:00:54 +0100

    change method interface

--- src/main/java/Currency/infranstructure/CurrencyLocaleService.java
+++ src/main/java/Currency/infranstructure/CurrencyLocaleService.java
@@ -1,16 +1,17 @@
 package Currency.infranstructure;
 
-
 import Currency.domain.model.entity.CurrencyLocale;
+import org.springframework.stereotype.Component;
 import retrofit2.Call;
-
+import java.io.IOException;
 import java.util.List;
 
+@Component
 public interface CurrencyLocaleService {
 
     Call<List<CurrencyLocaleService>> getCurrencyLocale();
     Call<List<CurrencyLocaleService>> getCurrencyLocaleByName(String name);
-    Call<Void> addProduct(CurrencyLocale currencyLocale);
+    Call<Void> addCurrencyLocale(CurrencyLocale currencyLocale) throws IOException, Exception;
 
 
 }
