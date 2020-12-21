7b739d2f97ebc45c6d46446c096830a1b3ec7110
LENOVO <luxus0@gmail.com>
2020-12-21 14:26:49 +0100

    change configDatabase method

--- src/main/java/Currency/configuration/ConfigDatabase.java
+++ src/main/java/Currency/configuration/ConfigDatabase.java
@@ -1,8 +1,12 @@
 package Currency.configuration;
 
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Configuration
 @PropertySources({
@@ -10,4 +14,7 @@ import org.springframework.context.annotation.PropertySources;
         @PropertySource("classpath:application-production.properties")
 })
 public class ConfigDatabase {
+
+
+
 }
