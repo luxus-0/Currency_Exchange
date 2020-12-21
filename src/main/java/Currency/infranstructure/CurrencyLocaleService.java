5fc3dc1b1b4d988b8791ec196adc97ce50ec8634
LENOVO <luxus0@gmail.com>
2020-12-21 16:18:16 +0100

    add method getCurrencyLocale getCurrencyLocaleByName addCurrencyLocale

--- src/main/java/Currency/infranstructure/CurrencyLocaleService.java
+++ src/main/java/Currency/infranstructure/CurrencyLocaleService.java
@@ -3,7 +3,6 @@ package Currency.infranstructure;
 import Currency.domain.model.entity.CurrencyLocale;
 import org.springframework.stereotype.Component;
 import retrofit2.Call;
-import java.io.IOException;
 import java.util.List;
 
 @Component
@@ -11,7 +10,7 @@ public interface CurrencyLocaleService {
 
     Call<List<CurrencyLocaleService>> getCurrencyLocale();
     Call<List<CurrencyLocaleService>> getCurrencyLocaleByName(String name);
-    Call<Void> addCurrencyLocale(CurrencyLocale currencyLocale) throws IOException, Exception;
+    Call<Void> addCurrencyLocale(CurrencyLocale currencyLocale);
 
 
 }
