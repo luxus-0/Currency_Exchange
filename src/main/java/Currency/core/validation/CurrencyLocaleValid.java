28c6748ac9d5c3fcdc836b2d3519b1616c4f2ad1
LENOVO <luxus0@gmail.com>
2020-12-21 16:19:29 +0100

    create class validation

---
+++ src/main/java/Currency/core/validation/CurrencyLocaleValid.java
@@ -0,0 +1,21 @@
+package Currency.core.validation;
+
+import Currency.infranstructure.CurrencyLocaleService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+public class CurrencyLocaleValid {
+
+    public void valid(List<CurrencyLocaleService> currencies) {
+        if (currencies != null) {
+            log.info(currencies);
+            currencies.forEach(System.out::println);
+        } else {
+            throw new RuntimeException("Currency locale is empty");
+        }
+    }
+}
