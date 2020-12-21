d0d1c26d0bb3c3f993a181964d836810751f21d0
LENOVO <luxus0@gmail.com>
2020-12-21 14:06:02 +0100

    add function getLocaleCurrency

---
+++ src/main/java/Currency/domain/service/CurrencyLocale.java
@@ -0,0 +1,32 @@
+package Currency.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Currency;
+import java.util.List;
+import java.util.Locale;
+
+@Service
+public class CurrencyLocale {
+
+    public List<Currency> getLocaleCurrency()
+    {
+        List<Currency> currencies = List.of(Currency.getInstance(Locale.UK));
+
+        for(int i = 0; i < currencies.size(); i++) {
+            if (currencies.get(i).toString().equals("")) {
+                currencies.stream()
+                        .filter(p -> p.getSymbol().length() > 0)
+                        .forEach(System.out::println);
+            }
+            else if(currencies.get(i).toString().isEmpty())
+            {
+                currencies.stream().filter((p) ->
+                {
+                    throw new RuntimeException("Currency Locale is empty");
+                }).forEach(System.out::println);
+            }
+        }
+        return currencies;
+    }
+}
