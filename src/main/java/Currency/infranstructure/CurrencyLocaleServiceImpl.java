42b6c6089ec7ac9059866b13c7c6f494daca35e2
LENOVO <luxus0@gmail.com>
2020-12-21 16:02:36 +0100

    add method getCurrencyLocale getCurrencyLocaleByName addCurrencyLocale

---
+++ src/main/java/Currency/infranstructure/CurrencyLocaleServiceImpl.java
@@ -0,0 +1,40 @@
+package Currency.infranstructure;
+
+import Currency.domain.client.RetrofitClient;
+import Currency.domain.model.entity.CurrencyLocale;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import retrofit2.Response;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyLocaleServiceImpl {
+
+    private RetrofitClient client;
+
+    public Response<List<CurrencyLocaleService>> getCurrencyLocale() throws Exception {
+        CurrencyLocaleService service = client.getRetrofitClient().create(CurrencyLocaleService.class);
+        Response<List<CurrencyLocaleService>> response = service.getCurrencyLocale().execute();
+        List<CurrencyLocaleService> currencies = response.body();
+        currencies.forEach(System.out::println);
+
+        return Response.success(currencies);
+    }
+
+    public Response<List<CurrencyLocaleService>> getCurrencyLocaleByName(String name) throws Exception {
+        CurrencyLocaleService service = client.getRetrofitClient().create(CurrencyLocaleService.class);
+        Response<List<CurrencyLocaleService>> response = service.getCurrencyLocaleByName(name).execute();
+        List<CurrencyLocaleService> currencies = response.body();
+        currencies.forEach(System.out::println);
+
+        return Response.success(currencies);
+    }
+
+    public void addCurrencyLocale(CurrencyLocale currencyLocale) throws Exception {
+        CurrencyLocaleService service = client.getRetrofitClient().create(CurrencyLocaleService.class);
+        service.addCurrencyLocale(new CurrencyLocale(1L,"USD")).execute();
+    }
+}
