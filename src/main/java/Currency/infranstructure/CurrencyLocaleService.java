85d5bb8b0790a7d6d90c2c731c043fca650dc5d9
LENOVO <luxus0@gmail.com>
2020-12-21 15:09:30 +0100

    remove annotation retrofit

--- src/main/java/Currency/infranstructure/CurrencyLocaleService.java
+++ src/main/java/Currency/infranstructure/CurrencyLocaleService.java
@@ -1,24 +1,16 @@
 package Currency.infranstructure;
 
 
+import Currency.domain.model.entity.CurrencyLocale;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 import java.util.List;
 
 public interface CurrencyLocaleService {
 
-    @GET("/getCurrenciesLocale")
-    Call<List<Currency.domain.service.CurrencyLocaleService>> getCurrencyLocale();
-
-    @GET("/getCurrenciesLocale/{name}")
-    Call<List<Currency.domain.service.CurrencyLocaleService>> getCurrencyLocaleByName(@Path("name") String name);
-
-    @POST("/addCurrenciesLocale")
-    Call<Void> addProduct(@Body Currency.domain.service.CurrencyLocaleService currencyLocale);
+    Call<List<CurrencyLocaleService>> getCurrencyLocale();
+    Call<List<CurrencyLocaleService>> getCurrencyLocaleByName(String name);
+    Call<Void> addProduct(CurrencyLocale currencyLocale);
 
 
 }
