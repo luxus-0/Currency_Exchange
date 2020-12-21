6d527926d59a448460ad619a20096ae651c423b1
LENOVO <luxus0@gmail.com>
2020-12-21 15:59:54 +0100

    add method to CurrencyApiRetrofit

--- src/main/java/Currency/api/CurrencyApiRetrofit.java
+++ src/main/java/Currency/api/CurrencyApiRetrofit.java
@@ -2,34 +2,36 @@ package Currency.api;
 
 import Currency.domain.model.entity.CurrencyLocale;
 import Currency.infranstructure.CurrencyLocaleService;
+import Currency.infranstructure.CurrencyLocaleServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
-import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyApiRetrofit {
 
-    private final CurrencyLocaleService currencyLocaleService;
+    private CurrencyLocaleServiceImpl currencyLocaleService;
 
     @GET("/getCurrenciesLocale")
-    public Call<List<CurrencyLocaleService>> getCurrenciesLocale() {
+    public Response<List<CurrencyLocaleService>> getCurrenciesLocale() throws Exception {
         return currencyLocaleService.getCurrencyLocale();
     }
 
     @GET("/getCurrenciesLocale/{name}")
-    public Call<List<CurrencyLocaleService>> getCurrenciesLocaleByName(@Path("name") String name) {
+    public Response<List<CurrencyLocaleService>> getCurrenciesLocaleByName(@Path("name") String name) throws Exception {
         return currencyLocaleService.getCurrencyLocaleByName(name);
     }
 
     @POST("/addCurrenciesLocale")
-    public Call<Void> addCurrenciesLocale(@Body CurrencyLocale currencyLocale) {
-        return currencyLocaleService.addProduct(currencyLocale);
+    public void addCurrenciesLocale(@Body CurrencyLocale currencyLocale) throws Exception {
+        currencyLocaleService.addCurrencyLocale(currencyLocale);
     }
 }
