8683d0cab6a4973ab3492bd4309b42f3cf3c873d
LENOVO <luxus0@gmail.com>
2020-12-22 17:12:31 +0100

    add currencyJson and getUrlNbp

--- src/main/java/Currency/domain/service/CurrencyNbp.java
+++ src/main/java/Currency/domain/service/CurrencyNbp.java
@@ -3,33 +3,36 @@ package Currency.domain.service;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
-
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 @Log4j2
 public class CurrencyNbp {
 
-    public ClientResponse getCurrencyBuyingAndSell(char table)
+    private final CurrencyUrl url;
+    public String getCurrencyBuyingAndSell(char table)
     {
         Client nbp = Client.create();
-        WebResource source  = nbp.resource("http://api.nbp.pl/api/exchangerates/tables/table/");
+        WebResource source  = nbp.resource(url.getUrlNbp(table));
         ClientResponse response = source.accept("application/json").get(ClientResponse.class);
+        String currencyJson = response.getEntity(String.class);
         if(response.getStatus() != 200)
         {
-            log.info("Error status code: " + response.getStatusInfo());
+            log.info("Error status code: " + response.getStatus());
         }
         else
         {
-            Set.of(response)
+            Set.of(currencyJson)
                     .stream()
-                    .filter(p -> p.getStatus() == 200)
+                    .filter(p -> response.getStatus() == 200)
                     .collect(Collectors.toSet()).forEach(System.out::println);
         }
-
-        return response;
+        return currencyJson;
     }
 }
