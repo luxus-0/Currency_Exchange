f5c00734c63da27bb7d3db6243054edd4624e976
LENOVO <luxus0@gmail.com>
2020-12-22 13:08:07 +0100

    create method getCurrencyBuyingAndSell

--- src/main/java/Currency/domain/service/CurrencyNbp.java
+++ src/main/java/Currency/domain/service/CurrencyNbp.java
@@ -6,19 +6,30 @@ import com.sun.jersey.api.client.WebResource;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @Log4j2
 public class CurrencyNbp {
 
-    public void getActualCurrency()
+    public ClientResponse getCurrencyBuyingAndSell(char table)
     {
         Client nbp = Client.create();
-        WebResource source  = nbp.resource("http://api.nbp.pl/api/exchangerates/tables/{table}/");
+        WebResource source  = nbp.resource("http://api.nbp.pl/api/exchangerates/tables/table/");
         ClientResponse response = source.accept("application/json").get(ClientResponse.class);
         if(response.getStatus() != 200)
         {
             log.info("Error status code: " + response.getStatusInfo());
         }
+        else
+        {
+            Set.of(response)
+                    .stream()
+                    .filter(p -> p.getStatus() == 200)
+                    .collect(Collectors.toSet()).forEach(System.out::println);
+        }
 
+        return response;
     }
 }
