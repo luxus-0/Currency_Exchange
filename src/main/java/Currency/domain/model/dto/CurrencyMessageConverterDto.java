881527ae7f58a37f3c933a7ac032d180a908b6b7
LENOVO <luxus0@gmail.com>
2020-12-23 14:53:00 +0100

    add CurrencyMessageConverterDto

---
+++ src/main/java/Currency/domain/model/dto/CurrencyMessageConverterDto.java
@@ -0,0 +1,19 @@
+package Currency.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CurrencyMessageConverterDto {
+
+    private String fromCurrency;
+    private String toCurrency;
+    private String display;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate createdDate;
+}
