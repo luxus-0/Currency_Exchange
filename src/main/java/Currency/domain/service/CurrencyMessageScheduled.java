522fb25a70fda7308a30100eb1aed3d7bc4dd9ed
LENOVO <luxus0@gmail.com>
2020-12-23 15:35:18 +0100

    add method sendCurrencyMessage

---
+++ src/main/java/Currency/domain/service/CurrencyMessageScheduled.java
@@ -0,0 +1,24 @@
+package Currency.domain.service;
+
+import Currency.domain.model.dto.CurrencyMessageConverterDto;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@AllArgsConstructor
+public class CurrencyMessageScheduled {
+
+    private final SimpMessagingTemplate template;
+
+    public void sendCurrencyMessage()
+    {
+        CurrencyMessageConverterDto dateMessage = CurrencyMessageConverterDto.builder()
+                .createdDate(LocalDate.now())
+                .build();
+
+        template.convertAndSend("/topic/pushmessages",new CurrencyMessageConverterDto("USD","PLN","Conversion complete",dateMessage.getCreatedDate()));
+    }
+}
