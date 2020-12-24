9ef4e72da169c50ba1d312eab5a87871e6fde692
LENOVO <luxus0@gmail.com>
2020-12-24 15:18:20 +0100

    create class AccountResponse

---
+++ src/main/java/Currency/domain/model/dto/AccountResponse.java
@@ -0,0 +1,19 @@
+package Currency.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class AccountResponse {
+    private Long id;
+    private BigDecimal balance;
+    private String currency;
+    private Long userId;
+}
