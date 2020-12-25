29d28bf5f2aac2f9cc86892ede4c043ada57f1c8
LENOVO <luxus0@gmail.com>
2020-12-25 01:10:23 +0100

    add class FromAccountByAmountNotFoundException

---
+++ src/main/java/Currency/core/exception/FromAccountByAmountNotFoundException.java
@@ -0,0 +1,10 @@
+package Currency.core.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FromAccountByAmountNotFoundException extends RuntimeException {
+    public FromAccountByAmountNotFoundException(String fromAccount) {
+    }
+}
