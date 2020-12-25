216788132b7cb9469572c2c28a11f7e7d149b20d
LENOVO <luxus0@gmail.com>
2020-12-25 19:08:23 +0100

    create class TransferRepository

---
+++ src/main/java/Currency/infranstructure/TransferRepository.java
@@ -0,0 +1,9 @@
+package Currency.infranstructure;
+
+import Currency.domain.model.entity.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer,Long> {
+}
