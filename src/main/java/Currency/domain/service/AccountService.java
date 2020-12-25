8d85e4da22075624fc79cbd3148c3eca424e9ebc
LENOVO <luxus0@gmail.com>
2020-12-25 01:09:14 +0100

    add function transferMoney and validAmount

--- src/main/java/Currency/domain/service/AccountService.java
+++ src/main/java/Currency/domain/service/AccountService.java
@@ -1,5 +1,6 @@
 package Currency.domain.service;
 
+import Currency.core.exception.FromAccountByAmountNotFoundException;
 import Currency.domain.model.dto.AccountRequest;
 import Currency.domain.model.dto.AccountResponse;
 import Currency.domain.model.entity.Account;
@@ -7,14 +8,15 @@ import Currency.infranstructure.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
     private final AccountRepository repository;
 
-    public AccountResponse find(Long id)
-    {
+    public AccountResponse find(Long id) {
         return repository
                 .findById(id)
                 .map(account -> AccountResponse.builder()
@@ -26,8 +28,7 @@ public class AccountService {
                 .orElseThrow(() -> new IllegalArgumentException("Account with id: " + id + "not found!"));
     }
 
-    public void save(AccountRequest request)
-    {
+    public void save(AccountRequest request) {
         repository.save(
                 Account.builder()
                         .balance(request.getBalance())
@@ -35,4 +36,52 @@ public class AccountService {
                         .userId(request.getUserId())
                         .build());
     }
+
+    public void transferMoney(long fromAccountId, long toAccountId, BigDecimal amount) {
+        validAmount(amount);
+        if (fromAccountId == toAccountId) {
+            throw new IllegalArgumentException("Don't transfer to the same account");
+        }
+
+        Account fromAccount = repository
+                .findById(fromAccountId)
+                .map(account -> Account.builder()
+                        .balance(account.getBalance())
+                        .currency(account.getCurrency())
+                        .id(account.getId())
+                        .userId(account.getUserId())
+                        .build())
+                .orElseThrow(() -> new IllegalArgumentException("From Account id: " + fromAccountId + "not found!"));
+
+
+        Account toAccount = repository
+                .findById(toAccountId)
+                .map(account -> Account.builder()
+                        .balance(account.getBalance())
+                        .currency(account.getCurrency())
+                        .id(account.getId())
+                        .userId(account.getUserId())
+                        .build())
+                .orElseThrow(() -> new IllegalArgumentException("To Account id: " + toAccountId + "not found!"));
+
+    if(fromAccount.getBalance().subtract(amount).equals(fromAccount.getBalance().negate()))
+    {
+        throw new FromAccountByAmountNotFoundException("You haven't got any money to account in id:" +fromAccountId);
+    }
+    else
+    {
+        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
+        toAccount.setBalance(toAccount.getBalance().add(amount));
+    }
+    repository.save(fromAccount);
+    repository.save(toAccount);
+
+
+    }
+        public void validAmount (BigDecimal amount)
+        {
+            if (amount.equals(amount.negate())) {
+                throw new IllegalArgumentException("Amount must be positive number");
+            }
+        }
 }
