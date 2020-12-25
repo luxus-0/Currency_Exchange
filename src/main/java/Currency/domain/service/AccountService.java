091ebc7a319ec82537a09c4a4bc93d58c2fd5d29
LENOVO <luxus0@gmail.com>
2020-12-26 00:07:27 +0100

    Initial commit

--- src/main/java/Currency/domain/service/AccountService.java
+++ src/main/java/Currency/domain/service/AccountService.java
@@ -7,7 +7,6 @@ import Currency.domain.model.entity.Account;
 import Currency.infranstructure.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 
 @Service
@@ -24,6 +23,7 @@ public class AccountService {
                         .currency(account.getCurrency())
                         .id(account.getId())
                         .userId(account.getUserId())
+                        .rate(account.getRate())
                         .build())
                 .orElseThrow(() -> new IllegalArgumentException("Account with id: " + id + "not found!"));
     }
@@ -34,10 +34,12 @@ public class AccountService {
                         .balance(request.getBalance())
                         .currency(request.getCurrency())
                         .userId(request.getUserId())
+                        .rate(request.getRate())
                         .build());
     }
 
-    public void transferMoney(long fromAccountId, long toAccountId, BigDecimal amount) {
+    public void transferMoney(long fromAccountId, long toAccountId, BigDecimal rate, BigDecimal amount)
+    {
         validAmount(amount);
         if (fromAccountId == toAccountId) {
             throw new IllegalArgumentException("Don't transfer to the same account");
@@ -50,6 +52,7 @@ public class AccountService {
                         .currency(account.getCurrency())
                         .id(account.getId())
                         .userId(account.getUserId())
+                        .rate(account.getRate())
                         .build())
                 .orElseThrow(() -> new IllegalArgumentException("From Account id: " + fromAccountId + "not found!"));
 
@@ -61,6 +64,7 @@ public class AccountService {
                         .currency(account.getCurrency())
                         .id(account.getId())
                         .userId(account.getUserId())
+                        .rate(account.getRate())
                         .build())
                 .orElseThrow(() -> new IllegalArgumentException("To Account id: " + toAccountId + "not found!"));
 
@@ -70,8 +74,8 @@ public class AccountService {
     }
     else
     {
-        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
-        toAccount.setBalance(toAccount.getBalance().add(amount));
+        fromAccount.setBalance(fromAccount.getBalance().subtract(amount.multiply(rate)));
+        toAccount.setBalance(toAccount.getBalance().add(amount.multiply(rate)));
     }
     repository.save(fromAccount);
     repository.save(toAccount);
